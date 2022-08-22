8069bf9387d6bc530a70ceadf8d731ef663baacf
Jimxx22 <jaumegil22@gmail.com>
2022-08-22 10:35:38 +0200

    security

--- src/main/java/com/c4/ud26/security/SecurityConfig.java
+++ src/main/java/com/c4/ud26/security/SecurityConfig.java
@@ -1,5 +1,37 @@
 package com.c4.ud26.security;
 
-public class SecurityConfig {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception{
+		 http.csrf().disable().authorizeRequests()
+         .antMatchers("/login").permitAll() //permitimos el acceso a /login a cualquiera
+         .anyRequest().authenticated() //cualquier otra peticion requiere autenticacion
+         .and().addFilterBefore(new LoginFilter("/login", authenticationManager()), UsernamePasswordAuthenticationFilter.class)
+         .addFilterBefore(new JwtFilter(),UsernamePasswordAuthenticationFilter.class);
+	}
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        // Creamos una cuenta de usuario por default
+       auth.inMemoryAuthentication()
+               .withUser("root")
+               .password("{noop}root_1")
+               .roles("ADMIN");
+       
+       auth.inMemoryAuthentication()
+			   .withUser("jgil")
+			   .password("{noop}root_1")
+			   .roles("USER");
+    }
+	
 }
